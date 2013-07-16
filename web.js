f4958850202a00e12be0e593664023629876a581
Niimenz <derronjay@gmail.com>
2013-07-16 12:22:47 +0000

    Added index.html

--- web.js
+++ web.js
@@ -2,8 +2,13 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var string_buff = fs.readFileSync("index.html");
+
+var buff_content = new Buffer(string_buff, "utf-8");
+
+var buff_output = but.toString();
 app.get('/', function(request, response) {
-  response.send('Hello World  2!');
+  response.send(buff_output);
 });
 
 var port = process.env.PORT || 5000;
